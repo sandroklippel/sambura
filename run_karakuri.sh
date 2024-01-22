a2135cde45f5bb4af4eb7384fb330a622b43482d
Sandro Klippel <sandroklippel@gmail.com>
2024-01-22 18:23:05 -0300

    fix crontab line

--- run_karakuri.sh
+++ run_karakuri.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 #   This script is intended to be run karakuri automata in a cron job, e.g.:
-#   */30 * * * * /sambura/run_karakuri.sh &>> /sambura/run_karakuri_$(date +\%Y\%m\%dT\%H%\M).log
+#   */30 * * * * /sambura/run_karakuri.sh &>> /sambura/run_karakuri_$(date +\%Y\%m\%dT\%H\%M).log
 #
 # Depends:
 #   podman
